8ab032b3323b6b34ecdfa179d2d644557ec01a21
Ankita Goyal <goyalankita0325@gmail.com>
2025-02-15 22:55:18 +0530

    working on navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true, // Enable the app directory feature
+    },
+};
+
+// Use ES module export syntax
 export default nextConfig;
